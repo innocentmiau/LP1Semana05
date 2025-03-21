6bb6b754d920b05a3694718a65cc0a59c179367c
André <imandreleandro@gmail.com>
2025-03-21 11:46:04 +0000

    Addex first version of tux

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage image = new CanvasImage("tux.jpg");
+
+            image.MaxWidth(20);
+            
+            AnsiConsole.Write(image);
+        }
+    }
+}
