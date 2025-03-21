4e94db4109991b19daa3ad301bf8c3838ab8aafd
André <imandreleandro@gmail.com>
2025-03-21 11:55:08 +0000

    Added library usage from another local project of the same solution

--- LevelTwo/SoGoodClass.cs
+++ LevelTwo/SoGoodClass.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using SoGoodLib;
 using Spectre.Console;
 
 namespace LevelTwo
 {
-    public class Program
+    public class SoGoodClass
     {
         private static void Main(string[] args)
         {
+            
             string imagePath = args.Length > 0 ? args[0] : "tux.jpg";
             CanvasImage image = new CanvasImage(imagePath);
             int width = args.Length > 1 ? int.Parse(args[1]) : 24;
             image.MaxWidth(width);
             
+            Console.WriteLine(SoGoodLib.SoGoodClass.SoGoodMethod());
             AnsiConsole.Write(image);
         }
     }
