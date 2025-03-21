c41ec531af72edc7a80cd71803183bf9f1af87d4
André <imandreleandro@gmail.com>
2025-03-21 14:18:39 +0000

    Added WorkerTable

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Bogus;
+using Bogus.DataSets;
+using Spectre.Console;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int count = int.Parse(args[0]);
+            Randomizer.Seed = new Random(count);
+            Faker faker = new Faker("pt_PT");
+
+            Table table = new Table();
+            table.AddColumn("ID");
+            table.AddColumn("Name");
+            table.AddColumn("Job");
+            
+            for (int i = 0; i < count; i++)
+            {
+                Name name = faker.Name;
+                table.AddRow($"{(i + 1)}", $"{name.FirstName()} {name.LastName()}", name.JobTitle());
+            }
+            
+            AnsiConsole.Write(table);
+        }
+    }
+}
